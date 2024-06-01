92f47d86eef378112a35baa72662bcbef5eaee7b
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2024-06-01 12:30:04 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const iconeMaiuscula = document.querySelector("#icone01");
 const iconeMinuscula = document.querySelector("#icone02");  
 const iconeNumero = document.querySelector("#icone03");  
 const iconeCaractereEspecial = document.querySelector("#icone04");  
+const fraseNivel = document.querySelector('#fraseNivel');
+const barraNivel = document.querySelector('#barra');
 
 
 
@@ -19,16 +21,98 @@ botao.addEventListener("click", function () {
 senha.addEventListener("input", function(){
 	const senhaAtual = this.value;
 	
-	(senhaAtual.length >= 8) ? iconeNumeroCaracteres.classList.replace('fa-xmark', 'fa-check') : iconeNumeroCaracteres.classList.replace('fa-check','fa-xmark'); 
+	if (senhaAtual.length >= 8){
+		iconeNumeroCaracteres.classList.replace('fa-xmark', 'fa-check')
+ 		iconeNumeroCaracteres.style.color ="#63E6BE";
+	}else{
+		iconeNumeroCaracteres.classList.replace('fa-check','fa-xmark')
+		iconeNumeroCaracteres.style.color ="#EF4444";
+	}; 
 	
-	(/[A-Z]/.test(senhaAtual) == true) ? iconeMaiuscula.classList.replace('fa-xmark', 'fa-check') : iconeMaiuscula.classList.replace('fa-check','fa-xmark'); 
+	if (/[A-Z]/.test(senhaAtual) == true){	 	iconeMaiuscula.classList.replace('fa-xmark', 'fa-check') 
+		iconeMaiuscula.style.color ="#63E6BE";
+	}else{
+		iconeMaiuscula.classList.replace( 'fa-check','fa-xmark')
+		iconeMaiuscula.style.color ="#EF4444";
+	} 
 	
-	(/[a-b]/.test(senhaAtual) == true) ? iconeMinuscula.classList.replace('fa-xmark', 'fa-check') : iconeMinuscula.classList.replace('fa-check','fa-xmark');
+	if (/[a-b]/.test(senhaAtual) == true){	 	iconeMinuscula.classList.replace('fa-xmark', 'fa-check') 
+		iconeMinuscula.style.color ="#63E6BE";
+	}else{
+		iconeMinuscula.classList.replace( 'fa-check','fa-xmark')
+		iconeMinuscula.style.color ="#EF4444";
+	} 
 
-	(/\d/.test(senhaAtual) == true) ? iconeNumero.classList.replace('fa-xmark', 'fa-check') : iconeNumero.classList.replace('fa-check','fa-xmark'); 
+	if (/\d/.test(senhaAtual) == true) 	{	 
+		iconeNumero.classList.replace('fa-xmark', 'fa-check') 
+		iconeNumero.style.color ="#63E6BE";
+	}else{
+		iconeNumero.classList.replace( 'fa-check','fa-xmark')
+		iconeNumero.style.color ="#EF4444";
+	} 
 		 
 	
-	(/[!@#$%^&*(),.?":{}|<>]/.test(senhaAtual) == true) ? iconeCaractereEspecial.classList.replace('fa-xmark', 'fa-check') : iconeCaractereEspecial.classList.replace('fa-check','fa-xmark');
+	if (/[!@#$%^&*(),.?":{}|<>]/.test(senhaAtual) == true){		iconeCaractereEspecial.classList.replace('fa-xmark', 'fa-check') 
+		iconeCaractereEspecial.style.color ="#63E6BE";
+	}else{
+		iconeCaractereEspecial.classList.replace( 'fa-check','fa-xmark')
+		iconeCaractereEspecial.style.color ="#EF4444";
+	}
+
+	const nivelSenha = {
+		0: "Muito fraca",
+		1: "Fraca",
+		2: "Moderada",
+		3: "Forte",
+		4: "Muito forte"
+	}
+
+	let nivel = 0
+
+	if(senhaAtual.length >= 8) nivel++
+	if (/[A-Z]/.test(senhaAtual)) nivel++
+	if (/[a-b]/.test(senhaAtual)) nivel++
+	if (/\d/.test(senhaAtual)) nivel++
+	if (/[!@#$%^&*(),.?":{}|<>]/.test(senhaAtual)) nivel++	
+		
 	
-	console.log(senhaAtual);	
+	
+	fraseNivel.innerText = nivelSenha[nivel-1]; 
+	
+	if( nivel <= 0){ 
+		barra.style.width = 0 +  '%';
+		barra.style.background = 'white'; 
+	}
+
+	if( nivel === 1){ 
+		barra.style.width = 5 +  '%';
+		barra.style.background = '#EF4444'; 
+	}
+
+	if( nivel === 2){
+		barra.style.width = 20 + '%';
+		barra.style.background = '#EF4444';
+	}
+
+	if( nivel === 3){ 
+		barra.style.width = 40 + '%';
+		barra.style.background = '#FFD43B';
+	}
+
+	if( nivel === 4){
+ 		barra.style.width = 60 + '%';
+		barra.style.background = '#63E6BE';
+	}
+
+	if( nivel === 5){ 
+		barra.style.width = 80 + '%';
+		barra.style.background = '#63E6BE';
+	}
+
+
+
+
+	if (nivel <= 0) fraseNivel.innerText = " ";
+	
+	console.log(nivel, senhaAtual);	
 });
